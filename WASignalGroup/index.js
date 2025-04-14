e49897ff6d82a2f3563629d8982435dbb6ab7d4b
KagenouHere <92816209+kagenouReal@users.noreply.github.com>
2025-04-15 00:31:55 +0800

    Update index.js

--- WASignalGroup/index.js
+++ WASignalGroup/index.js
@@ -2,4 +2,4 @@ module.exports.GroupSessionBuilder = require('./group_session_builder')
 module.exports.SenderKeyDistributionMessage = require('./sender_key_distribution_message')
 module.exports.SenderKeyRecord = require('./sender_key_record')
 module.exports.SenderKeyName = require('./sender_key_name')
-module.exports.GroupCipher = require('./group_cipher')
+module.exports.GroupCipher = require('./group_cipher')//kage
